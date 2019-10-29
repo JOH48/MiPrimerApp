1f10222a3210c5d73af602ad0630b23774d6d469
JOH48 <13125029@galileo.edu>
2019-10-28 22:55:48 -0600

    CHANGE - Se cambio toda la parte del binding de la forma nativa hacia la utilizacion de butterknife.

--- app/src/main/java/com/android/miprimerapp/LoginActivity.java
+++ app/src/main/java/com/android/miprimerapp/LoginActivity.java
@@ -2,8 +2,6 @@ package com.android.miprimerapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.service.autofill.UserData;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -12,12 +10,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.miprimerapp.dataBase.UserDataBase;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
+public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.activity_main_btnLogin)
     Button btnLogin;
+    @BindView(R.id.activity_main_btnRegister)
     Button btnRegister;
+    @BindView(R.id.activity_main_txtUser)
     EditText txtUser;
+    @BindView(R.id.activity_main_txtPassword)
     EditText txtPassword;
 
 
@@ -25,61 +31,38 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        //dentro de este metodo... se une la parte grafica y la parte de codigo.
-        btnLogin = findViewById(R.id.activity_main_btnLogin);
-        btnRegister = findViewById(R.id.activity_main_btnRegister);
-        txtUser = findViewById(R.id.activity_main_txtUser);
-        txtPassword = findViewById(R.id.activity_main_txtPassword);
-
-
-        //Aca le indicamos en que clase ... se van a manejar los eventos.
-        btnLogin.setOnClickListener(this);
-        btnRegister.setOnClickListener(this);
+        ButterKnife.bind(this);
     }
 
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.activity_main_btnLogin:
-                //aca metemos todo lo que queremos que haga el boton login....
+    @OnClick(R.id.activity_main_btnLogin)
+    public void tryLogin() {
+        if (txtUser.getText().toString().trim().equals("") ||
+                txtPassword.getText().toString().trim().equals("")) {
+            Toast.makeText(this, "EL USUARIO Y CONTRASEÑA SON OBLIGATORIOS",
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            Boolean searchUser = UserDataBase.searchUsername(txtUser.getText().toString().trim());
 
-                if (txtUser.getText().toString().trim().equals("") ||
-                        txtPassword.getText().toString().trim().equals("")) {
-                    Toast.makeText(this, "EL USUARIO Y CONTRASEÑA SON OBLIGATORIOS",
-                            Toast.LENGTH_SHORT).show();
+            if (searchUser == true) {
+                if (UserDataBase.login(txtUser.getText().toString().trim(),
+                        txtPassword.getText().toString().trim()) == true) {
+                    startLandingActivity();
                 } else {
-                    Boolean searchUser = UserDataBase.searchUsername(txtUser.getText().toString().trim());
-
-                    if (searchUser == true ) {
-                        if (UserDataBase.login(txtUser.getText().toString().trim(),
-                                txtPassword.getText().toString().trim()) == true) {
-                            startLandingActivity();
-                        } else {
-                            Toast.makeText(this, "Contraseña es incorrecta", Toast.LENGTH_SHORT).show();
-                        }
-                    } else {
-                        Toast.makeText(this, "EL USUARIO NO EXISTE", Toast.LENGTH_SHORT).show();
-                    }
+                    Toast.makeText(this, "Contraseña es incorrecta", Toast.LENGTH_SHORT).show();
                 }
-
-                /*if(validateFields()==true && validateLogin()==true){
-                       if (UserDataBase.login(txtUser.getText().toString().trim(), txtPassword.getText().toString().trim()) == true) {
-                           startLandingActivity();
-                       } else {
-                           Toast.makeText(this, "El usuario o contraseña estan icorrectos", Toast.LENGTH_SHORT).show();
-                       }
-                }*/
-                break;
-            case R.id.activity_main_btnRegister:
-                //aca metemos todo lo que queremos que haga el boton register..
-                startRegisterActivity();
-                break;
-
+            } else {
+                Toast.makeText(this, "EL USUARIO NO EXISTE", Toast.LENGTH_SHORT).show();
+            }
         }
-
     }
+
+
+    @OnClick(R.id.activity_main_btnRegister)
+    public void tryRegister() {
+        startRegisterActivity();
+    }
+
 
     private Boolean validateLogin() {
         Boolean vlog = false;
--- app/src/main/java/com/android/miprimerapp/RegisterActivity.java
+++ app/src/main/java/com/android/miprimerapp/RegisterActivity.java
@@ -2,24 +2,32 @@ package com.android.miprimerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Build;
+
 import android.os.Bundle;
-import android.view.View;
-import android.view.accessibility.AccessibilityManager;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.android.miprimerapp.dataBase.UserDataBase;
 
-public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class RegisterActivity extends AppCompatActivity{
+    @BindView(R.id.register_activity_txtUser)
     EditText txtUser;
+    @BindView(R.id.register_activity_txtName)
     EditText txtName;
+    @BindView(R.id.register_activitytxtSecondName)
     EditText txtSecondName;
+    @BindView(R.id.register_activity_txtPassword)
     EditText txtPassword;
+    @BindView(R.id.register_activity_txtRepeatedPassword)
     EditText txtRepeatedPassword;
+    @BindView(R.id.register_activity_btnRegister)
     Button btnRegister;
+    @BindView(R.id.register_activity_btnCancel)
     Button btnCancel;
 
 
@@ -27,83 +35,71 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-
-        //Unimos los widget o vistas de la parte grafica con el codigo.
-        txtUser = findViewById(R.id.register_activity_txtUser);
-        txtName = findViewById(R.id.register_activity_txtName);
-        txtSecondName = findViewById(R.id.register_activitytxtSecondName);
-        txtPassword = findViewById(R.id.register_activity_txtPassword);
-        txtRepeatedPassword = findViewById(R.id.register_activity_txtRepeatedPassword);
-        btnRegister = findViewById(R.id.register_activity_btnRegister);
-        btnCancel = findViewById(R.id.register_activity_btnCancel);
-
-        //ACCEDEMOS AL METODO ESPECIAL DE LOS BOTONES.. QUE LE DA FUNCIONALIDAD.
-        btnRegister.setOnClickListener(this);
-        btnCancel.setOnClickListener(this);
-
+        ButterKnife.bind(this);
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.register_activity_btnCancel:
-                this.finish();
-                break;
-            case R.id.register_activity_btnRegister:
-                registerUser();
-                break;
-        }
+
+    @OnClick(R.id.register_activity_btnRegister)
+    public void tryRegister() {
+        registerUser();
     }
+
+
+    @OnClick(R.id.register_activity_btnCancel)
+    public void tryCancel() {
+        this.finish();
+    }
+
 
 
     //METODO QUE SE MANDA A LLAMAR CUANDO SE PRESIONA EL BOTON DE REGISTRO.
-    private void registerUser(){
-        if(validateFields()==true){
-            if(UserDataBase.searchUsername(txtUser.getText().toString().trim())==true){
-                Toast.makeText(this,"IMPOSIBLE REGISTRAR, EL USUARIO YA EXISTE",Toast.LENGTH_SHORT).show();
+    private void registerUser() {
+        if (validateFields() == true) {
+            if (UserDataBase.searchUsername(txtUser.getText().toString().trim()) == true) {
+                Toast.makeText(this, "IMPOSIBLE REGISTRAR, EL USUARIO YA EXISTE", Toast.LENGTH_SHORT).show();
             } else {
-                UserDataBase.insertUser(txtUser.getText().toString().trim(),txtName.getText().toString().trim(),
-                        txtSecondName.getText().toString().trim(),txtPassword.getText().toString().trim());
+                UserDataBase.insertUser(txtUser.getText().toString().trim(), txtName.getText().toString().trim(),
+                        txtSecondName.getText().toString().trim(), txtPassword.getText().toString().trim());
                 this.finish();
             }
         }
     }
 
 
-
     //sE VALIDA QUE TODOS LOS CAMPOS SEAN OBLIGATORIOS
     //DEVUELVE TRUE SI TODOS LOS CAMPOS ESTAN LLENOS Y DEVUELVE FALSE SI ALGUNO NO ESTA LLENO
     //O SI LAS CONTRASEÑAS NO SON IGUALES.
-    private Boolean validateFields(){
-        if(txtUser.getText().toString().trim().equals("")){
+    private Boolean validateFields() {
+        if (txtUser.getText().toString().trim().equals("")) {
             Toast.makeText(this, "EL CAMPO DE USUARIO NO PUEDE ESTAR VACIO",
                     Toast.LENGTH_SHORT).show();
             return false;
         }
-        if(txtName.getText().toString().trim().equals("")){
-            Toast.makeText(this,"EL CAMPO NOMBRE NO PUEDE ESTAR VACIO",
+        if (txtName.getText().toString().trim().equals("")) {
+            Toast.makeText(this, "EL CAMPO NOMBRE NO PUEDE ESTAR VACIO",
                     Toast.LENGTH_SHORT).show();
             return false;
         }
-        if(txtSecondName.getText().toString().trim().equals("")){
+        if (txtSecondName.getText().toString().trim().equals("")) {
             Toast.makeText(this, "EL CAMPO APELLIDO NO PUEDE ESTAR VACIOS",
                     Toast.LENGTH_SHORT).show();
             return false;
         }
-        if(txtPassword.getText().toString().trim().equals("")){
-            Toast.makeText(this,"EL CAMPO CONTRASEÑA NO PUEDE ESTAS VACIOS",
+        if (txtPassword.getText().toString().trim().equals("")) {
+            Toast.makeText(this, "EL CAMPO CONTRASEÑA NO PUEDE ESTAS VACIOS",
                     Toast.LENGTH_SHORT).show();
             return false;
         }
-        if(txtRepeatedPassword.getText().toString().trim().equals("")){
-            Toast.makeText(this,"EL CAMPO REPETIR CONTRASEÑA NO PUEDE ESTAS VACIO",
+        if (txtRepeatedPassword.getText().toString().trim().equals("")) {
+            Toast.makeText(this, "EL CAMPO REPETIR CONTRASEÑA NO PUEDE ESTAS VACIO",
                     Toast.LENGTH_SHORT).show();
             return false;
         }
-        if(!txtPassword.getText().toString().trim().equals(
+        if (!txtPassword.getText().toString().trim().equals(
                 txtRepeatedPassword.getText().toString().trim()
-        )){ Toast.makeText(this,"LAS CONTRASEÑAS NO SON IGUALES",
-                Toast.LENGTH_SHORT).show();
+        )) {
+            Toast.makeText(this, "LAS CONTRASEÑAS NO SON IGUALES",
+                    Toast.LENGTH_SHORT).show();
             return false;
         }
 
